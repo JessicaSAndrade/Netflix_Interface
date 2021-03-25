8154127e3c7ee97bcdd33b910c9499da40e43b68
jessica andrade <jessica.andrade@avantia.com.br>
2021-03-24 21:50:50 -0300

    subindo o projeto em estagio final para a tela inicial

---
+++ resources/js/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        400: {
+            items: 3
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
